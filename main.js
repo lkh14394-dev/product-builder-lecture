4a140397949d49032543a4321deff7a01cf7d1c5
lkh14394 <lkh14394@gmail.com>
2026-02-19 15:40:46 +0000

    Translate project to Korean and update theme toggle text

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const themeToggle = document.getElementById('theme-toggle');
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme === 'dark') {
     document.body.classList.add('dark-mode');
-    themeToggle.textContent = 'Light Mode';
+    themeToggle.textContent = '라이트 모드';
 }
 
 themeToggle.addEventListener('click', () => {
@@ -14,9 +14,9 @@ themeToggle.addEventListener('click', () => {
     let theme = 'light';
     if (document.body.classList.contains('dark-mode')) {
         theme = 'dark';
-        themeToggle.textContent = 'Light Mode';
+        themeToggle.textContent = '라이트 모드';
     } else {
-        themeToggle.textContent = 'Dark Mode';
+        themeToggle.textContent = '다크 모드';
     }
     localStorage.setItem('theme', theme);
 });
